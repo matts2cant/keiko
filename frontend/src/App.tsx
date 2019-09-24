02d5988840fcd6a18d4629554efc7c9a722e693e
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-24 14:50:31 +0200

    Change style of pokedex

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { Route } from 'react-router';
 import { Store } from 'redux';
 import { Persistor } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
+import { createGlobalStyle } from "styled-components";
 
 import Root from './components/Root';
 import Routes from './routes';
@@ -23,8 +24,19 @@ const RootComponentWithRoutes: React.FunctionComponent = () => (
   </Root>
 );
 
+const CustomFont = createGlobalStyle`
+  @font-face {
+    font-family: 'pokemon_gb';
+    src: url('font/pokemon_gb-webfont.woff2') format('woff2'),
+         url('font/pokemon_gb-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
 const App: React.FunctionComponent<Props> = ({ history, persistor, store }) => (
   <Provider store={store}>
+    <CustomFont />
     <PersistGate loading={null} persistor={persistor}>
       <ConnectedRouter history={history}>
         <Route path="/" component={RootComponentWithRoutes} />
--- frontend/src/pages/Home/Home.style.tsx
+++ frontend/src/pages/Home/Home.style.tsx
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 export default {
   Intro: styled.div`
-    font-size: 20px;
+    text-align: center;
+    font-family: pokemon_gb;
+    margin: 20px;
+  `,
+  Title: styled.h1`
+    font-size: 42px;
+  `,
+  Container: styled.div`
     display: flex;
     align-items: center;
-    flex-direction: column;
-    padding: 10px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
   `,
 };
