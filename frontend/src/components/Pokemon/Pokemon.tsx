bc7cc11e518c6373474bbda176a8b87bbe556c24
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-24 15:18:36 +0200

    Shorter syntax for pokemon component

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Style from './Pokemon.style';
 import {FormattedMessage} from "react-intl";
+import Style from './Pokemon.style';
 
 interface Props {
   name: string;
@@ -9,24 +9,22 @@ interface Props {
   weight: number;
 }
 
-class Pokemon extends React.Component<Props> {
-  render(): React.ReactNode {
-    return (
-      <Style.PokeBox>
-        <Style.Title>
-          {this.props.name}
-        </Style.Title>
-        <Style.Picture>
-          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`} alt={this.props.name}/>
-        </Style.Picture>
-        <Style.Info>
-          <li><FormattedMessage id="pokemon.id" />: {this.props.id}</li>
-          <li><FormattedMessage id="pokemon.height" />: {this.props.height} cm</li>
-          <li><FormattedMessage id="pokemon.weight" />: {this.props.weight / 10} kg</li>
-        </Style.Info>
-      </Style.PokeBox>
-    );
-  }
+function Pokemon(props: Props) {
+  return (
+    <Style.PokeBox>
+      <Style.Title>
+        {props.name}
+      </Style.Title>
+      <Style.Picture>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>
+      </Style.Picture>
+      <Style.Info>
+        <li><FormattedMessage id="pokemon.id" />: {props.id}</li>
+        <li><FormattedMessage id="pokemon.height" />: {props.height} cm</li>
+        <li><FormattedMessage id="pokemon.weight" />: {props.weight / 10} kg</li>
+      </Style.Info>
+    </Style.PokeBox>
+  );
 }
 
 export default Pokemon;
