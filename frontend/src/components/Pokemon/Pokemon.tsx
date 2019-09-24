3ca22c14d37147f302dc16246bda878af587b02c
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-24 16:10:40 +0200

    Add button to flip pokemons

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {FormattedMessage} from "react-intl";
 import Style from './Pokemon.style';
 
@@ -10,13 +11,19 @@ interface Props {
 }
 
 function Pokemon(props: Props) {
+  const [flipped, setFlipped] = useState(false);
+
   return (
     <Style.PokeBox>
       <Style.Title>
         {props.name}
       </Style.Title>
+      <Style.FlipButton>
+        <img onClick={() => setFlipped(!flipped)} src="flip.svg" alt="Flip pokemon"/>
+      </Style.FlipButton>
       <Style.Picture>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>
+        {!flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>}
+        {flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`} alt={props.name}/>}
       </Style.Picture>
       <Style.Info>
         <li><FormattedMessage id="pokemon.id" />: {props.id}</li>
