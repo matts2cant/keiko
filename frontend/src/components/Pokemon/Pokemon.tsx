59c69e43613b823773489540744ab0ecde7e53ef
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-24 17:30:10 +0200

    Add details page when clicking on a pokemon

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { useState } from 'react';
 import {FormattedMessage} from "react-intl";
 import Style from './Pokemon.style';
+import {Link} from "react-router-dom";
 
 interface Props {
+  full: boolean;
   name: string;
   id: number;
   height: number;
@@ -14,23 +16,32 @@ function Pokemon(props: Props) {
   const [flipped, setFlipped] = useState(false);
 
   return (
-    <Style.PokeBox>
-      <Style.Title>
-        {props.name}
-      </Style.Title>
-      <Style.FlipButton>
-        <img onClick={() => setFlipped(!flipped)} src="flip.svg" alt="Flip pokemon"/>
-      </Style.FlipButton>
-      <Style.Picture>
-        {!flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>}
-        {flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`} alt={props.name}/>}
-      </Style.Picture>
-      <Style.Info>
-        <li><FormattedMessage id="pokemon.id" />: {props.id}</li>
-        <li><FormattedMessage id="pokemon.height" />: {props.height} cm</li>
-        <li><FormattedMessage id="pokemon.weight" />: {props.weight / 10} kg</li>
-      </Style.Info>
-    </Style.PokeBox>
+      <Style.PokeBox>
+        <Style.Title>
+          <Link to={`/pokemon/${props.id}`}>
+            {props.name}
+          </Link>
+        </Style.Title>
+        <Style.FlipButton>
+          <img onClick={() => setFlipped(!flipped)} src="/flip.svg" alt="Flip pokemon"/>
+        </Style.FlipButton>
+        {!props.full && <Style.Picture>
+          {!flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>}
+          {flipped && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`} alt={props.name}/>}
+        </Style.Picture>}
+        {props.full && <Style.Picture>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name}/>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.id}.png`} alt={props.name}/>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${props.id}.png`} alt={props.name}/>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${props.id}.png`} alt={props.name}/>
+        </Style.Picture>}
+
+        <Style.Info>
+          <li><FormattedMessage id="pokemon.id" />: {props.id}</li>
+          <li><FormattedMessage id="pokemon.height" />: {props.height} cm</li>
+          <li><FormattedMessage id="pokemon.weight" />: {props.weight / 10} kg</li>
+        </Style.Info>
+      </Style.PokeBox>
   );
 }
 
---
+++ frontend/src/pages/Details/Details.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+
+import Pokemon from 'components/Pokemon';
+import {FormattedMessage} from 'react-intl';
+import {RouteComponentProps} from "react-router";
+import {makeGetRequest} from "services/networking/request";
+import HomeStyle from './../Home/Home.style';
+import Style from './Details.style';
+
+interface State {
+  loading: boolean,
+  error: boolean,
+  pokemon: {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+  };
+}
+interface Params {
+  id: string;
+}
+
+class Details extends React.Component<RouteComponentProps<Params>, State> {
+  constructor(props: RouteComponentProps<Params>) {
+    super(props);
+    this.state = {loading: true, error: false, pokemon: { id: 0, name: "", height: 0, weight: 0}};
+  }
+
+  async componentDidMount() {
+    try {
+      const id = this.props.match.params.id;
+      const response = await makeGetRequest("/pokemon/" + id);
+      this.setState({
+        loading: false,
+        error: false,
+        pokemon: {
+          id: response.body.id,
+          name: response.body.name,
+          height: response.body.height,
+          weight: response.body.weight,
+        },
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+        error: true,
+        pokemon: { id: 0, name: "", height: 0, weight: 0},
+      })
+    }
+  }
+
+  render(): React.ReactNode {
+    const loader = <img src={"/loader.svg"} alt={"loader"}/>;
+
+    return (
+        <HomeStyle.Intro>
+            <HomeStyle.Title><FormattedMessage id="pokemon.pokedex" /></HomeStyle.Title>
+            <HomeStyle.Container>
+              {this.state.loading && loader}
+              {!this.state.loading && <Pokemon full={true} id={this.state.pokemon.id} name={this.state.pokemon.name} height={this.state.pokemon.height} weight={this.state.pokemon.weight}/>}
+              {this.state.error && <HomeStyle.Error><FormattedMessage id="pokemon.error" /></HomeStyle.Error>}
+            </HomeStyle.Container>
+        </HomeStyle.Intro>
+    );
+  }
+}
+
+export default Details;
