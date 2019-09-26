d3b7681ab9162c614fa217ce56e26779728d5657
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-26 13:22:17 +0200

    Add tests

--- frontend/src/__fixtures__/state.ts
+++ frontend/src/__fixtures__/state.ts
@@ -1,1 +1,42 @@
-export const state = {};
+export const state = {
+  pokemon: {
+    detailedPokemon: {
+      id: 1,
+      name: 'bulbasaur',
+      height: 7,
+      weight: 69
+    },
+    pokemons: {
+      '31': {
+        id: 31,
+        name: 'nidoqueen',
+        height: 13,
+        weight: 600
+      },
+      '32': {
+        id: 32,
+        name: 'nidoran-m',
+        height: 5,
+        weight: 90
+      },
+      '33': {
+        id: 33,
+        name: 'nidorino',
+        height: 9,
+        weight: 195
+      },
+      '34': {
+        id: 34,
+        name: 'nidoking',
+        height: 14,
+        weight: 620
+      },
+      '35': {
+        id: 35,
+        name: 'clefairy',
+        height: 6,
+        weight: 75
+      },
+    },
+  },
+};
