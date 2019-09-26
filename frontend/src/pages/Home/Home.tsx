8a68df5cc11c11aa3aea3eeee6529dc0394e643f
Matthieu de Canteloube <matts2cant@gmail.com>
2019-09-26 10:15:52 +0200

    Fix pagination

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -11,7 +11,6 @@ import {normalize} from "services/PokemonNormalizer";
 import Style from './Home.style';
 
 interface State {
-  page: number;
   loading: boolean;
   error: boolean;
 }
@@ -28,7 +27,7 @@ interface RouteParams {
 class Home extends React.Component<Props<RouteParams>, State> {
   constructor(props: Props<RouteParams>) {
     super(props);
-    this.state = {page: 1, loading: false, error: false};
+    this.state = {loading: false, error: false};
   }
 
   async componentDidMount() {
@@ -36,12 +35,11 @@ class Home extends React.Component<Props<RouteParams>, State> {
     this.updateData(page);
   }
 
-  async componentDidUpdate(prevProps: RouteComponentProps<RouteParams>, prevState: State) {
+  async componentDidUpdate(prevProps: Props<RouteParams>, prevState: State) {
     const prevPage = Number(prevProps.match.params.page || 1);
     const page = Number(this.props.match.params.page || 1);
     if (prevPage !== page) {
       await this.updateData(page);
-
     }
   }
 
@@ -51,13 +49,13 @@ class Home extends React.Component<Props<RouteParams>, State> {
       this.props.fetchPokemonsSuccess({
         pokemons: normalize(response.body),
       });
-      return response.body;
     } catch (e) {
       console.error(`An error occurred in the Home component: ${e}`);
     }
   }
 
   render(): React.ReactNode {
+    debugger;
     const pokemonComponents = this.props.pokemons.map((data: PokemonType) =>
       <Pokemon {...data} key={data.id.toString()} detailedView={false} />
     );
@@ -65,13 +63,15 @@ class Home extends React.Component<Props<RouteParams>, State> {
     const loader = <img src={"/loader.svg"} alt={"loader"}/>;
     const error = <Style.Error><FormattedMessage id="pokemon.error" /></Style.Error>;
 
+    const page = Number(this.props.match.params.page || 1);
+
     return (
       <Style.Intro>
         <Style.Title><FormattedMessage id="pokemon.pokedex" /></Style.Title>
         <Style.Paginator>
-          <Link to={`/pokedex/${this.state.page - 1}`}>&laquo;</Link>
-          {this.state.page}
-          <Link to={`/pokedex/${this.state.page + 1}`}>&raquo;</Link>
+          {page > 1 && <Link to={`/pokedex/${page - 1}`}>&laquo;</Link>}
+          {page}
+          {page < 6 && <Link to={`/pokedex/${page + 1}`}>&raquo;</Link>}
         </Style.Paginator>
         <Style.Container>
           {this.state.loading && loader}
